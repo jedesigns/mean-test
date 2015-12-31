7a8958521fdfa540cb10a3af5fc0e0f8dc7a87d0
jedesigns <josh@jedesigns.co.uk>
2015-12-31 12:20:52 +0000

    update port

--- server/server.js
+++ server/server.js
@@ -6,6 +6,4 @@ var debug = require('debug')('passport-mongo'),
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
-});
+var server = app.listen(3000);
